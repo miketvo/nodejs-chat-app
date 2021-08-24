196435bf8cd0a75ee8c3416225ecf2ef8a9a28a8
miketvo <ikxplicitie@gmail.com>
2021-08-24 11:00:01 +0700

    Finished all basic features

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,35 +1,69 @@
+const socket = io();
 const messageContainer = $("#message-container");
+const nameBox = $("#name-box");
+const chatBox = $("#chat-box");
 const url = window.location.origin;
 console.log("Client Javascript running on", url);
 
 $(() => {
   $("#send").click(() => {
     sendMessage({
-      name: $("#name-box").val(),
-      content: $("#chat-box").val(),
+      socketID: socket.id,
+      name: nameBox.val(),
+      content: chatBox.val(),
     });
+    chatBox.val("");
   });
   getMessages();
 });
 
-const updateMessages = messages => {
+const updateMessages = data => {
   messageContainer.innerHTML = "";
-  messages.forEach(message => {
-    messageContainer.append(`
-      <div class="msg-in">
-        <div class="name">${message.name}</div>
-        <div class="msg">${message.content}</div>
-      </div>`
-    );
-  });
+  if (data.forEach) {
+    data.forEach(message => {
+      if (message.socketID === socket.id) {
+        messageContainer.append(`
+          <div class="msg-out">
+            <div class="name">${message.name}</div>
+            <div class="msg">${message.content}</div>
+          </div>`
+        );
+      } else {
+        messageContainer.append(`
+          <div class="msg-in">
+            <div class="name">${message.name}</div>
+            <div class="msg">${message.content}</div>
+          </div>`
+        );
+      }
+    });
+  } else {
+    if (data.socketID === socket.id) {
+      messageContainer.append(`
+        <div class="msg-out">
+          <div class="name">${data.name}</div>
+          <div class="msg">${data.content}</div>
+        </div>`
+      );
+    } else {
+      messageContainer.append(`
+        <div class="msg-in">
+          <div class="name">${data.name}</div>
+          <div class="msg">${data.content}</div>
+        </div>`
+      );
+    }
+  }
 }
 
 const getMessages = () => {
-  $.get(url + "/messages", messages => {
-    updateMessages(messages);
+  $.get(url + "/messages", data => {
+    updateMessages(data);
   });
 }
 
 const sendMessage = message => {
   $.post(url + "/messages", message);
 }
+
+socket.on("send-message", updateMessages);
--- server.js
+++ server.js
@@ -2,13 +2,21 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const http = require("http").createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(http);
 
 uri = "mongodb://localhost/nodejs-chat-app";
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}, error => {
   console.log("Database connected");
 });
 
+io.on("connection", socket => {
+  console.log("A user is connected", socket.id);
+});
+
 const Message = mongoose.model("Message", {
+  socketID: String,
   name: String,
   content: String,
 });
@@ -21,7 +29,7 @@ app.get("/messages", (req, res) => {
   Message.find({}, (error, messages) => {
     res.send(messages);
     console.log(`GET ALL | ERR = ${error} | at ${Date()}`);
-  })
+  });
 });
 
 app.post("/messages", (req, res) => {
@@ -29,9 +37,10 @@ app.post("/messages", (req, res) => {
   message.save(error => {
     console.log(`POST | ERR = ${error} | at ${Date()}`);
   });
+  io.emit("send-message", req.body);
   res.send(message);
 });
 
-const server = app.listen(8080, "localhost", () => {
+const server = http.listen(8080, "0.0.0.0", () => {
   console.log("Server is running on: ", server.address());
 });
